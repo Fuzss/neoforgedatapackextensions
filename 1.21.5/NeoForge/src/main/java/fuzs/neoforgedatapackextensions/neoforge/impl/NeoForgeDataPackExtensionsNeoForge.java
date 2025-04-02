788f80db016e403ff417a4db7f2337924a290a12
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-04-03 00:23:30 +0200

    prepare 1.21.5 port

---
+++ 1.21.5/NeoForge/src/main/java/fuzs/neoforgedatapackextensions/neoforge/impl/NeoForgeDataPackExtensionsNeoForge.java
@@ -0,0 +1,12 @@
+package fuzs.neoforgedatapackextensions.neoforge.impl;
+
+import fuzs.neoforgedatapackextensions.impl.NeoForgeDataPackExtensions;
+import net.neoforged.fml.common.Mod;
+
+@Mod(NeoForgeDataPackExtensions.MOD_ID)
+public class NeoForgeDataPackExtensionsNeoForge {
+
+    public NeoForgeDataPackExtensionsNeoForge() {
+        // NO-OP
+    }
+}
